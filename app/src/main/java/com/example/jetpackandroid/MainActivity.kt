b9219bc42920a02f447a64a92ff6377834b71aed
himanshu <himanshub1295@gmail.com>
2022-11-12 11:10:11 +0530

    live data dependency added live data complete

--- app/src/main/java/com/example/jetpackandroid/MainActivity.kt
+++ app/src/main/java/com/example/jetpackandroid/MainActivity.kt
@@ -4,45 +4,54 @@ import android.icu.number.Precision.increment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Log.i
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var textView : TextView
-    lateinit var counterBtn : Button
-
     lateinit var mainViewModel: MainViewModel
+
+    lateinit var textView : TextView
+    lateinit var button: Button
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainViewModel = ViewModelProvider(this, MainViewModelFactory(10)).get(MainViewModel::class.java)
-        // yha pe MainViewModelFactory ki help se initial value bhej skte hai
-        // parametr bhej skte hai ViewModelFactory ki help se
-
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         textView = findViewById(R.id.counterLabel)
-        counterBtn = findViewById(R.id.counterBtn)
-        setText()
+        button = findViewById(R.id.counterBtn)
 
+        CoroutineScope(Dispatchers.Main).launch {
 
-
-        counterBtn.setOnClickListener{
-            mainViewModel.increment()
-            setText()
+            for (i in 1..10) {
+                delay(1000)
+                mainViewModel.counter = i
+                mainViewModel.updateLiveData()
+            }
         }
 
-        // yha sirf button click vgerah handle hore hai bss data to viewmodel se aara h
+        // OBSERVER
+        mainViewModel.factsLiveData.observe(this, Observer{
+            textView.text = it
 
-    }
+            // factslive data pe observer lga diya
+            // jese hi data mein koi change hoga yha pe jo code likha hoga vo execute hoga
 
+        })
 
-
-    fun setText() {
-        textView.text = mainViewModel.count.toString()
+        button.setOnClickListener{
+//            mainViewModel.updateLiveData()
+        }
 
     }
 
--- app/src/main/java/com/example/jetpackandroid/MainViewModel.kt
+++ app/src/main/java/com/example/jetpackandroid/MainViewModel.kt
@@ -1,15 +1,29 @@
 package com.example.jetpackandroid
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel(val initalValue : Int) : ViewModel() {
+class MainViewModel: ViewModel() {
+    var counter = 0
 
-    // view model mein sirf data rkhte hai ya data pe kaam krte h
-    // kisi view ka referene ni lete
+    private val factsLiveDataObj = MutableLiveData<String>("this is a fact")
 
-    var count : Int = initalValue
+    // yha bss data ko livedata se enclose krdiya
+    // ab main activity mein observer lga denge
 
-    fun increment() {
-        count++
+    val factsLiveData : LiveData<String> = factsLiveDataObj
+
+    // upr vaale ko prvate krdiya usko bss hum yha change kar paaye taaki
+    // baki puri application iss vale object ko access kregi kyuki ye read only hai
+    // upr vaale mein changes hum apne hisaab se krenge yha for eg
+    // repository se data laa k facts live data obj mein store krdenge
+    // isse security bdhti hai
+    // ni to koi b change krdeta isko ya glti se hum hi chnage krde kya pta kyuki same sa naam h
+    // isliye private krdiya
+
+
+    suspend fun updateLiveData() {
+        factsLiveDataObj.value = counter.toString()
     }
 }
